661e9a55a5bc8420c92fb087914449cd1a21d490
Juan Fernández <juan@acyclic.co>
2022-03-26 03:32:49 +0100

    fix: typescript module name chore: version bumped

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
-declare module "react-styled-flexboxgrid" {
-  import { StyledComponent, DefaultTheme, ThemedCssFunction } from "styled-components";
+declare module "@sitecloud/react-styled-flexboxgrid" {
+  import {
+    StyledComponent,
+    DefaultTheme,
+    ThemedCssFunction,
+  } from "styled-components";
 
   namespace ReactStyledFlexboxgrid {
     interface ITheme {
@@ -80,15 +84,31 @@ declare module "react-styled-flexboxgrid" {
       mdOffset?: number;
       lgOffset?: number;
     }
-
   }
 
   export type ITheme = ReactStyledFlexboxgrid.ITheme;
-  export const Grid: StyledComponent<"div", any, ReactStyledFlexboxgrid.IGridProps, never>;
-  export const Row: StyledComponent<"div", any, ReactStyledFlexboxgrid.IRowProps, never>;
-  export const Col: StyledComponent<"div", any, ReactStyledFlexboxgrid.IColProps, never>;
+  export const Grid: StyledComponent<
+    "div",
+    any,
+    ReactStyledFlexboxgrid.IGridProps,
+    never
+  >;
+  export const Row: StyledComponent<
+    "div",
+    any,
+    ReactStyledFlexboxgrid.IRowProps,
+    never
+  >;
+  export const Col: StyledComponent<
+    "div",
+    any,
+    ReactStyledFlexboxgrid.IColProps,
+    never
+  >;
 
-  export const config: (props: any) => {
+  export const config: (
+    props: any
+  ) => {
     media: {
       xs: ThemedCssFunction<DefaultTheme>;
       sm: ThemedCssFunction<DefaultTheme>;
